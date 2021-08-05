bf9e931c076639bdfb6a0ef7bb6272c36e4cee21
lucasdesenvolvedor <lucas_aragao_fel@hotmail.com>
2021-08-05 10:20:14 -0300

    Scripts de DML e DQL

---
+++ sprint-1-bd/exercicios/manha/1.1-exercicio-pessoas/1.1-exercicio-pessoas_DML.sql
@@ -0,0 +1,16 @@
+USE EMPRESA;
+GO 
+
+INSERT INTO PESSOA (nomePessoa)
+VALUES ('SAULO'), ('LUCAS');
+
+INSERT INTO TELEFONE (idPessoa,numeroTelefone)
+VALUES (1,'9999'), (1,'8888'), (2,'7777');
+
+INSERT INTO EMAIL (idPessoa, end_email)
+VALUES (1,'s.santos@email.com'), 
+(2,'c.zaneti@email.com');
+
+INSERT INTO CNH (idPessoa, descricao) 
+VALUES (1,'1234'), (2,'4334');
+
