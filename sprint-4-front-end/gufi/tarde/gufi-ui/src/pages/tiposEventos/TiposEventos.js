bdaa2998b0d858dd02b38f6f160d236b1fbf597d
lucasdesenvolvedor <lucas_aragao_fel@hotmail.com>
2021-10-22 07:51:48 -0300

    consulta api tipoeventos

--- sprint-4-front-end/gufi/tarde/gufi-ui/src/pages/tiposEventos/TiposEventos.js
+++ sprint-4-front-end/gufi/tarde/gufi-ui/src/pages/tiposEventos/TiposEventos.js
@@ -4,14 +4,92 @@ export default class TiposEventos extends Component{
     constructor(props){
         super(props);
         this.state = {
-            listaTiposEventos : [ { idTipoEvento : 1, titulo : 'C#' }, { idTipoEvento : 2, titulo : 'SQL' }, { idTipoEvento : 3, titulo : 'ReactJS' } ],
+            listaTiposEventos : [],
             titulo : ''
         };
     };
 
+   
+    buscarTiposEventos = () => {
+        console.log("agora vamos fazer a chamada para a API")
+
+     //Informamos o end point.
+
+    fetch('http://localhost:5000/api/TipoEventos')  //nessa linha vamos 
+                                       //enviar a requiscao
+     //por padrao será um GET.
+     
+
+     //entao, quando vc tiver um retorno, pegue essa resposta e deixe
+     //em formato JSON.
+
+     //Define o tipo de dados do retorno da aplicacao como JSON.
+
+     //THEN = ENTÃO;
+
+    .then( resposta => resposta.json())
+
+     //THEN = ENTÃO;
+
+    ////atualizado o statis listaTiposEventos com os dados obtidos.
+    .then( dados => this.setState({ listaTiposEventos: dados }) )
+  
+    //caso ocorra algum erro, mostra no console do computador.
+    .catch(erro => console.log(erro))
+
+    //.then( resposta => resposta.json())
+
+    //.then( dados => console.log(dados) )
+
+
+    /*fetch('http://localhost:5000/api/TipoEventos').then( resposta => resposta.json())
+    .then( dados => this.setState({ listaTiposEventos: dados }) )
+    .catch(erro => console.log(erro))*/
+
+
+    }
+
+
     componentDidMount(){
-        //
+          this.buscarTiposEventos()
+                  //
     };
+
+    //onChange vai disparar por tecla e invocar essa funcao.
+    atualizaEstadodoTitulo = async (event) => {
+
+                         //Nome titulo > valor input.
+        await this.setState({  titulo: event.target.value })
+         console.log(this.state.titulo)
+
+    }
+
+
+    cadastrarTipoEvento = (event) => {
+        event.preventDefault();
+
+        fetch('http://localhost:5000/api/TipoEventos', {
+
+               method:'POST',
+               //body: { tituloTipoEvento = this.state.titulo } //lembrando que aqui é o objeto JS, e nao JSON;
+
+               body: JSON.stringify({ tituloTipoEvento : this.state.titulo }),
+               headers:{ "Content-Type" :"application/json"}})
+
+            .then(console.log("Tipo de evento cadastrado."))
+
+                   //caso ocorra algum erro, mostra no console do navegador.
+            .catch(erro => console.log(erro))
+
+             //entao, atualiza a lista de tipos de evento.
+
+            .then(this.buscarTiposEventos)
+
+            .then(this.setState({ titulo: ''}))
+
+    }
+
+
 
     render(){
         return(
@@ -35,7 +113,7 @@ export default class TiposEventos extends Component{
                                         return(
                                             <tr key={tipoEvento.idTipoEvento}>
                                                 <td>{tipoEvento.idTipoEvento}</td>
-                                                <td>{tipoEvento.titulo}</td>
+                                                <td>{tipoEvento.tituloTipoEvento}</td>
                                             </tr>
                                         )
                                     } )                                 
@@ -43,6 +121,34 @@ export default class TiposEventos extends Component{
                             </tbody>
                         </table>
                     </section>
+
+                   <section>
+                          <h2>Cadastro de tipo de evento.</h2>
+                          <form  onSubmit={this.cadastrarTipoEvento}>
+                              <div>
+                                    <input type="text" 
+
+                                    //atualiza o tipo de input
+                                    //valor do status é do input.
+                                    value={this.state.titulo}  
+                                    
+                                    placeholder="Titulo do Tipo de Evento"
+
+                                    //cada vez que tiver uma mudanca (por tecla)
+                                    onChange={this.atualizaEstadodoTitulo}                                    
+                                    
+                                    />
+
+                                    {/** quando clicar em cadastrar temo que submeter o formulario. */}
+                                    <button type="submit" >Cadastrar</button>
+                              </div>                   
+
+                          </form>                    
+
+
+
+                   </section>
+
 
                     {/* Cadastro de tipos de eventos */}
                 </main>
