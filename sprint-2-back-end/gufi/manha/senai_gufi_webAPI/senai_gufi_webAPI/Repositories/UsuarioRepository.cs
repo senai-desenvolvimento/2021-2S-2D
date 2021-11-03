6f372e7f33c7ac58b9931d90f315ef1dfa29e660
lucasdesenvolvedor <lucas_aragao_fel@hotmail.com>
2021-11-03 20:39:13 -0300

    controllers eventos, tipos usuarios e tipo eventos - gufi api -  manha e tarde

--- sprint-2-back-end/gufi/manha/senai_gufi_webAPI/senai_gufi_webAPI/Repositories/UsuarioRepository.cs
+++ sprint-2-back-end/gufi/manha/senai_gufi_webAPI/senai_gufi_webAPI/Repositories/UsuarioRepository.cs
@@ -109,5 +109,123 @@ namespace senai_gufi_webAPI.Repositories
                 foto.CopyTo(stream);
             }
         }
+
+
+
+        /// <summary>
+        /// Atualiza um usuário existente
+        /// </summary>
+        /// <param name="id">ID do usuário que será atualizado</param>
+        /// <param name="usuarioAtualizado">Objeto com as novas informações</param>
+        public void Atualizar(int id, Usuario usuarioAtualizado)
+        {
+            // Busca um usuário através do id
+            Usuario usuarioBuscado = ctx.Usuarios.Find(id);
+
+            // Verifica se o nome do usuário foi informado
+            if (usuarioAtualizado.NomeUsuario != null)
+            {
+                // Atribui os novos valores ao campos existentes
+                usuarioBuscado.NomeUsuario = usuarioAtualizado.NomeUsuario;
+            }
+
+            // Verifica se o e-mail do usuário foi informado
+            if (usuarioAtualizado.Email != null)
+            {
+                // Atribui os novos valores ao campos existentes
+                usuarioBuscado.Email = usuarioAtualizado.Email;
+            }
+
+            // Verifica se a senha do usuário foi informado
+            if (usuarioAtualizado.Senha != null)
+            {
+                // Atribui os novos valores ao campos existentes
+                usuarioBuscado.Senha = usuarioAtualizado.Senha;
+            }
+
+            // Atualiza o tipo de usuário que foi buscado
+            ctx.Usuarios.Update(usuarioBuscado);
+
+            // Salva as informações para serem gravadas no banco
+            ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Busca um usuário através do ID
+        /// </summary>
+        /// <param name="id">ID do usuário que será buscado</param>
+        /// <returns>Um usuário buscado</returns>
+        public Usuario BuscarPorId(int id)
+        {
+            // Retorna o primeiro usuário encontrado para o ID informado, sem exibir sua senha
+            return ctx.Usuarios
+                .Select(u => new Usuario()
+                {
+                    IdUsuario = u.IdUsuario,
+                    NomeUsuario = u.NomeUsuario,
+                    Email = u.Email,
+                    IdTipoUsuario = u.IdTipoUsuario,
+
+                    IdTipoUsuarioNavigation = new TipoUsuario()
+                    {
+                        IdTipoUsuario = u.IdTipoUsuarioNavigation.IdTipoUsuario,
+                        TituloTipoUsuario = u.IdTipoUsuarioNavigation.TituloTipoUsuario
+                    }
+                })
+                .FirstOrDefault(u => u.IdUsuario == id);
+        }
+
+        /// <summary>
+        /// Cadastra um novo usuário
+        /// </summary>
+        /// <param name="novoUsuario">Objeto novoUsuario que será cadastrado</param>
+        public void Cadastrar(Usuario novoUsuario)
+        {
+            // Adiciona este novoUsuario
+            ctx.Usuarios.Add(novoUsuario);
+
+            // Salva as informações para serem gravadas no banco de dados
+            ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Deleta um usuário existente
+        /// </summary>
+        /// <param name="id">ID do usuário que será deletado</param>
+        public void Deletar(int id)
+        {
+            // Remove o tipo de usuário que foi buscado
+            ctx.Usuarios.Remove(BuscarPorId(id));
+
+            // Salva as alterações
+            ctx.SaveChanges();
+        }
+
+        /// <summary>
+        /// Lista todos os usuários
+        /// </summary>
+        /// <returns>Uma lista de usuários</returns>
+        public List<Usuario> Listar()
+        {
+            // Retorna uma lista com todas as informações dos tipos de usuários, exceto as senhas
+            return ctx.Usuarios
+                .Select(u => new Usuario()
+                {
+                    IdUsuario = u.IdUsuario,
+                    NomeUsuario = u.NomeUsuario,
+                    Email = u.Email,
+                    IdTipoUsuario = u.IdTipoUsuario,
+
+                    IdTipoUsuarioNavigation = new TipoUsuario()
+                    {
+                        IdTipoUsuario = u.IdTipoUsuarioNavigation.IdTipoUsuario,
+                        TituloTipoUsuario = u.IdTipoUsuarioNavigation.TituloTipoUsuario
+                    }
+                })
+                .ToList();
+        }
+
+      
+       
     }
 }
