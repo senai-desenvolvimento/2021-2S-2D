2fa9ce5f28e7db11c9dcde322b5bcc21279cd4a8
Saulo Santos <saulo.macedoprojetos@gmail.com>
2021-11-05 15:49:46 -0300

    Atualiza string de conexao

--- sprint-2-back-end/gufi/tarde/senai_gufi_webAPI/senai_gufi_webAPI/Context/GufiContext.cs
+++ sprint-2-back-end/gufi/tarde/senai_gufi_webAPI/senai_gufi_webAPI/Context/GufiContext.cs
@@ -33,7 +33,7 @@ namespace senai_gufi_webAPI.Context
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 //optionsBuilder.UseSqlServer("Data Source=DESKTOP-U20H53U; Initial Catalog=gufi_tarde; user id=sa; pwd=Senai@132;");
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-U20H53U; Initial Catalog=gufi_tarde; user id=sa; pwd=Senai@132;");
+                optionsBuilder.UseSqlServer("Data Source=DESKTOP-30RGV41\\SQLEXPRESS; Initial Catalog=gufi_tarde; user id=sa; pwd=senai@132;");
             }
         }
 
